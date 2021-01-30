ebda7a31460e97fa48777b3e34b86006cbde93d8
OGUZHAN MAVI <oguzhan.mavi17@ogr.atauni.edu.tr>
2021-01-31 02:20:12 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+   public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // İş kodları ...
+            // Bir iş Sınıfı başka sınıfları new yapmaz unutma önemli !
+            //yetkisi var mı ?
+
+            return _productDal.GetAll();
+        }
+    }
+}
